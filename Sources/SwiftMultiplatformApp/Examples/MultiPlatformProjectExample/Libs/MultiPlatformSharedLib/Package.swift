25e17630c23cb4975064722b79dffd162416d971
Hugo Gonzalez <hagonzalez94@gmail.com>
2023-07-18 16:18:42 -0700

    update ci + remove wasm example

--- Sources/SwiftMultiplatformApp/Examples/MultiPlatformProjectExample/Libs/MultiPlatformSharedLib/Package.swift
+++ Sources/SwiftMultiplatformApp/Examples/MultiPlatformProjectExample/Libs/MultiPlatformSharedLib/Package.swift
@@ -14,7 +14,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(name: "Example", dependencies: ["MultiPlatformSharedLib"]),
         .target(
             name: "MultiPlatformSharedLib"),
         .testTarget(
